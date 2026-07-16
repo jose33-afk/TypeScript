e5330cfff5d43e5e489efd0451ed1dfadde178a2
jose33-afk <josedev337@gmail.com>
2026-07-15 21:20:38 -0300

    chore: commit inicial do ambiente de estudos
    - Motivo: criar o repositório base para praticar e aprender TypeScript
    
    - Ação: inicializado o projeto e criadas as configurações básicas

---
+++ Type Annotation e inference 1/exercicio.js
@@ -0,0 +1,32 @@
+"use strict";
+// Exercicio 1.
+/*
+function normalizarTexto(texto: string) {
+  return texto.trim().toLowerCase();
+}
+
+let nome = 'LeanDRO ';
+console.log(normalizarTexto(nome));
+*/
+// Exercicio 2.
+const input = document.querySelector('input'); // 2.0
+const total = localStorage.getItem('Total') ?? ''; // 1.0 
+input.value = total;
+calcularGanho(+input.value);
+function calcularGanho(value) {
+    value = +value;
+    const p = document.querySelector('p');
+    if (p)
+        p.innerText = `Ganho total: ${value + 100 - value * 0.2}`;
+}
+function totalMudou() {
+    const value = Number(input.value);
+    localStorage.setItem('Total', String(value));
+    calcularGanho(value);
+}
+input?.addEventListener('keyup', totalMudou);
+/*
+  1.0 - ??: Se o lado esquerdo for null ou undefined use o lado direito.
+  2.0 - as: O typeScript não consegue ler o DOM. então ele assume que vc buscou um elmento generioc
+        e como neste caso é um input ele contém .value, mas um elemento generico não possui, dessa forma ele reclama.
+*/ 
