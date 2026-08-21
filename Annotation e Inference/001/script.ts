60a5afdd425e606cc611db916011441a14cda92b
jose33-afk <josedev337@gmail.com>
2026-08-20 21:10:55 -0300

    feat: configura ambiente TS e introduz annotations e inference
    - Motivo: Necessidade de (re)configurar as ferramentas base do TypeScript e registrar os primeiros fundamentos da linguagem.
    
    - Ação: Instalação do setup inicial e criação dos primeiros exemplos práticos demonstrando tipagem explícita (annotations) e implícita (inference).

---
+++ Annotation e Inference/001/script.ts
@@ -0,0 +1,35 @@
+let produto= 'Livro';
+const preco = 200;
+
+const carro = {
+  marca: 'Audi',
+  portas: 5,
+}
+
+const barato = preco < 400 ? true : 'Produto é caro';
+
+function somar(a: number, b:number) {
+  return a + b;
+}
+
+// somar('3', 3)
+
+const nitendo = {
+  nome: 'Nitendo',
+  preco: '2000',
+}
+
+function transformarPreco(produto: { nome: string; preco: string }) { // 1.0
+  return produto.preco = 'R$' + produto.preco;
+}
+
+console.log(transformarPreco(nitendo));
+
+// const teste = { nome:'2', preco:'3'}
+// console.log(transformarPreco(teste))
+
+
+/*
+  1.0 - Muito interresante... Ele proprio faz as verificações de; tipo, e se um determinado item,
+        Tem ou não certa propriedade. Isso vai me poupar bastante tempo.
+*/
