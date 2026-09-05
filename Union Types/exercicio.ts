9415e6c032e846b61afe4c979894281c82bcbe3e
jose33-afk <josedev337@gmail.com>
2026-09-05 15:21:13 -0300

    feat: adiciona pasta e exercícios de Union Types
    - Motivo: Registrar a prática que encerra o tópico de Union Types.
    
    - Ação: Criado o diretório do módulo e adicionados os exercícios resolvidos.

---
+++ Union Types/exercicio.ts
@@ -0,0 +1,20 @@
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+// 3 - Se a função receber um número, retorne um número
+// 4 - Se a função receber uma string, retorne um número
+// 5 - Se ela receber algo diferente, retorne um erro.
+//  (throw "value deve ser um número ou uma string")
+
+function toNumber(value: number | string) {
+  if (typeof value === 'number') return value;
+
+  if (typeof value === 'string') {
+    const num = Number(value);
+    if (!isNaN(num)) return num;
+  };
+
+  throw new Error("value deve ser um número ou uma string");
+}
+
+
+console.log(toNumber('h'))
---
+++ Union Types/script.js
@@ -0,0 +1,9 @@
+let total = 200;
+total = "4000";
+function isNumber(value) {
+    if (typeof value === "number")
+        return true;
+    return false;
+}
+const button = document.querySelector('button');
+export {};
