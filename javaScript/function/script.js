394b40bb3c62c8bc58931cf0ec21caee804611aa
jose33-afk <josedev337@gmail.com>
2026-09-19 19:25:40 -0300

    feat: adiciona testes e estudos sobre call, apply, bind e new
    - Motivo: Necessidade de lapidar a base de JavaScript puro, entendendo a fundo a manipulação do 'this' e os métodos nativos de funções.
    
    - Ação: Adicionados exemplos práticos e comentados sobre o funcionamento de call, apply, bind e criação de callbacks (meuMap) no arquivo de estudos de JS.

--- javaScript/function/script.js
+++ javaScript/function/script.js
@@ -1,17 +1,25 @@
+// Conhecendo mais new.
+
+/*
 const perimetro = new Function('lado', 'return lado * 4');
 
 function somar(n1, n2) {
   return n1 + n2;
 }
 
-// console.log(somar)
+console.log(somar)
+*/
 
+
+// Entendendo call.
+
+/*
 function descricaoCarro(carro) {
   console.log(this)
   console.log(this.marca + ' ' + carro);
 }
 
-// descricaoCarro.call({marca: 'Honda', ano: 2010}, 'Civi') // 1.0
+descricaoCarro.call({marca: 'Honda', ano: 2010}, 'Civi') // 1.0
 
 function Dom(seletor) {
   this.element = document.querySelector(seletor);
@@ -21,22 +29,26 @@ Dom.prototype.ativo = function (classe) {
   this.element.classList.add(classe);
 }
 
-// const li = {
-//   element: document.querySelector('li'),
-// }
+const li = {
+  element: document.querySelector('li'),
+}
 
-// const ul = new Dom('ul');
+const ul = new Dom('ul');
 
-// Dom.prototype.ativo.call(li, 'Ativo')
-// console.log(ul)
+Dom.prototype.ativo.call(li, 'Ativo')
+console.log(ul)
 
 const frutas = ['Uva', 'Maçã', 'Banana'];
 
 Array.prototype.pop.call(frutas);
-// console.log(frutas)
+console.log(frutas)
+*/
 
+
+// Analizando bind.
+
+/*
 const li = document.querySelectorAll('li');
-
 
 const filtro = Array.prototype.filter.bind(li, (item) => {
   return item.classList.contains('ativo');
@@ -44,37 +56,68 @@ const filtro = Array.prototype.filter.bind(li, (item) => {
 
 const numeros = [2, 45, 3, 4, 5, 67];
 
-// console.log(Math.max.apply(null, numeros))
+console.log(Math.max.apply(null, numeros))
 
-// console.log(filtro())
+*/
 
-// const obj = {
-//   nome: 'Ana',
-//   falar() {
-//     console.log(`Ola, sou ${this.nome}`);
-//   }
-// }
+/*
+const obj = {
+  nome: 'Ana',
+  falar() {
+    console.log(`Ola, sou ${this.nome}`);
+  }
+}
 
-// function teste() {
-//   return obj.falar.bind(obj);
-// }
+function teste() {
+  return obj.falar.bind(obj);
+}
 
-// setTimeout(teste(), 3000);
+setTimeout(teste(), 3000);
+*/
 
-// function meuMap(callback) {
-//   const novoArray = [];
-//   for (let i = 0; i < this.length; i++) {
-//     novoArray.push(callback(this[i]))
-//   }
-//   return novoArray
-// }
 
-// const arrytest = ['Abacaxi', 'mamao', 'Abobora']
+// Entendendo o funcinamento do map, callback, call e apply.
 
-// const novoArray = meuMap.call(arrytest, item => item + ' vendido');
+/*
+function meuMap(callback) {
+  const novoArray = [];
+  for (let i = 0; i < this.length; i++) {
+    novoArray.push(callback(this[i]))
+  }
+  return novoArray
+}
 
-// console.log(novoArray)
+const arrytest = ['Abacaxi', 'mamao', 'Abobora']
 
+const novoArray = meuMap.call(arrytest, item => item + ' vendido');
+
+console.log(novoArray)
+*/
+
+
+// --- Duas arrays: 1 - array com funcões em ordem de execução.
+//     2 - funcoes com os argumentos que eu quero na mesma ordem da array de- 
+//     funções. 
+//     
+//     Retorno: - uma array com os argumentos e função já passados, faltando somente
+//     ativar. usando resultados[0]() ---
+
+/*
+const arrayfunction = [
+  function(fruta){ console.log(`1 ${fruta}`)},
+  function(fruta){ console.log(`2 ${fruta}`)},
+  function(fruta){ console.log(`3 ${fruta}`)}
+]
+
+const frutas = ['mamao', 'maca', 'Abobora']
+
+function processar(fn1, fn2, fn3) {
+  return [fn1, fn2, fn3].map((funcao, index) => funcao.bind(null, this[index]));
+}
+
+const resultados = processar.apply(frutas, arrayfunction);
+resultados[0]()
+*/
 
 /*
   1 - o call: muda o this, originalmente as funcoes tem como this o windown,
